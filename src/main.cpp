9859ea6369db1da5e61ca0414fcd9d5a33bcc954
Marián Hlaváč <hlavac95@gmail.com>
2018-03-06 13:52:21 +0100

    Change output to CSV format

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,11 @@
 #include <vector>
 #include <stack>
 
-#define BUFFER_MAX  256
+#define BUFFER_MAX      256
 #ifndef SHRT_MAX 
-#define SHRT_MAX 32767 
+#define SHRT_MAX        32767 
 #endif
+#define P_DELIM         ","
 
 const short KNIGHT_MOVES = 8;
 const short KNIGHT_MOVES_COORDS[8][2] = { 
@@ -91,59 +92,69 @@ class Game {
             return std::make_pair(pos % dimension, pos / dimension);
         }
 
-        std::string dump_coords(short pos) {
-            std::stringstream ss;
-            ss << "" << pos / dimension << ":" << pos % dimension << "";
-            return ss.str();
+        std::string to_coords_str(short pos) {
+            std::pair<short, short> coords = to_coords(pos);
+            return "(" + std::to_string(coords.first) + 
+                "," + std::to_string(coords.second) + ")";
         }
 };
 
-struct Solution {
-    std::vector<short> path;
-    short pieces_left;
-    short upper_bound = SHRT_MAX;
-    bool valid = false;
+class Solution {
+    protected:
+        std::vector<short> path;
+        Game* game = nullptr;
 
-    Solution(short pieces, short upper_bound) : 
-        pieces_left(pieces), valid(true), upper_bound(upper_bound) { }
+    public:
+        short upper_bound = SHRT_MAX;
+        bool valid = false;
+        short pieces_left;
 
-    Solution(short upper_bound) : valid(false), upper_bound(upper_bound) { }
+        Solution(Game* game) : 
+            pieces_left(game->pieces), valid(true), 
+            upper_bound(game->upper_bound), game(game) { }
 
-    Solution(const Solution* s) {
-        path = s->path;
-        pieces_left = s->pieces_left;
-        valid = s->valid;
-        upper_bound = s->upper_bound;
-    }
+        Solution(short upper_bound) : 
+            upper_bound(upper_bound) { }
 
-    /**
-     * Gets the price of this solution.
-     */
-    size_t get_size() const {
-        return valid ? path.size() : upper_bound;
-    }
+        Solution(const Solution* s) : 
+            path(s->path), pieces_left(s->pieces_left), valid(s->valid), 
+            upper_bound(s->upper_bound), game(s->game) { }
 
-    /**
-     * Gets the last node on the path.
-     */
-    short get_last() const {
-        return path.back();
-    }
-
-    /**
-     * Dumps the contents of the struct for debugging purposes.
-     */
-    std::string dump() {
-        if (!valid) return "Solution {\n  Invalid\n}";
-        std::stringstream ss;
-        ss << "Solution {" << std::endl << "  path: ";
-        for (short node : path) {
-            ss << node << ", ";
+        /**
+         * Gets the price of this solution.
+         */
+        size_t get_size() const {
+            return valid ? path.size() : upper_bound;
         }
-        ss << std::endl << "  pieces_left: " << pieces_left << "," << std::endl;
-        ss << "}" << std::endl;
-        return ss.str();
-    }
+
+        /**
+         * Gets the last node on the path.
+         */
+        short get_last() const {
+            return path.back();
+        }
+
+        /**
+         * Adds another node to the path.
+         */
+        void add_node(short node) {
+            path.push_back(node);
+        }
+
+        bool is_in_path(short node) {
+            return std::find(path.begin(), path.end(), node) != path.end();
+        }
+
+        std::string dump() {
+            std::string dump = std::string(valid ? "valid" : "invalid") + P_DELIM + std::to_string(upper_bound) +
+                P_DELIM + std::to_string(get_size()) + P_DELIM;
+
+            for (short coord : path) {
+                dump += game->to_coords_str(coord) + ";";
+            }
+
+            return dump;
+        }
 };
 
 class Solver {
@@ -171,11 +182,11 @@ class Solver {
 
                 Solution* next = new Solution(current);
                 short coords = last + move[0] + dim * move[1];
-                next->path.push_back(coords);
+                next->add_node(coords);
 
                 // Check if stepped on a piece
                 if (game->is_piece_at(coords)) {
-                    if (find(current->path.begin(), current->path.end(), coords) == current->path.end()) {
+                    if (!current->is_in_path(coords)) {
                         next->pieces_left--;
                     }
                 }
@@ -185,6 +196,7 @@ class Solver {
         }
 
     public:
+        long iterations = 0;
         Solver(Game* instance) : game(instance) { }
 
         /**
@@ -197,19 +209,16 @@ class Solver {
             std::vector<Solution*> steps;
             Solution* best_solution = new Solution(game->upper_bound + 1);
 
-            Solution* root = new Solution(game->pieces, game->upper_bound);
-            root->path.push_back(game->start_coord);
+            Solution* root = new Solution(game);
+            root->add_node(game->start_coord);
             stack.push_back(root);
 
-            long count = 0;
-
             while (!stack.empty()) {
-                count++;
+                iterations++;
                 Solution* node = stack.back();
                 stack.pop_back();
-                
-	            // ---- FIXME:
-                // Check if not off limits or useless (pruning)
+
+                // Prune
                 if (node->get_size() + node->pieces_left >= best_solution->get_size()) {
                     continue;
                 }
@@ -227,10 +236,7 @@ class Solver {
                         stack.push_back(next);
                     }
                 }
-                // ---- FIXME:
             }
-
-            std::cout << "Iterations: " << count << std::endl;
 
             for (Solution* sol : stack) { delete sol; }
             stack.clear();
@@ -245,20 +251,25 @@ int main(int argc, char** argv) {
         std::cerr << "usage: " << argv[0] << " [filename]" << std::endl;
         return 64;
     }
-    const char* filename = argv[1];
 
-    // Execute
-    // try {
-        Game game = Game::create_from_file(filename);
-        Solver solver(&game);
+    // Print CSV header
+    std::cout << "filename,validity,upper_bound,solution_length,solution,iterations" << std::endl;
 
-        Solution solution = solver.solve();
+    // Execute and print out results
+    try {
+        for (int i = 1; i < argc; i++) {
+            Game game = Game::create_from_file(argv[i]);
+            Solver solver(&game);
 
-        std::cout << "Solution is: " << solution.dump() << std::endl;
-    // } 
-    // catch (std::runtime_error err) {
-    //     std::cerr << err.what();
-    // }
+            Solution solution = solver.solve();
+
+            std::cout << argv[i] << P_DELIM << solution.dump() << P_DELIM <<
+                solver.iterations << std::endl;
+        }
+    } 
+    catch (std::runtime_error err) {
+        std::cerr << err.what();
+    }
 
     // Clean up
 
