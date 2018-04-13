7ae2e15739c8264e656dc367738e9b4dc7c3a4d2
Marián Hlaváč <hlavac95@gmail.com>
2018-04-13 16:40:53 +0200

    Fix the testing utility

--- src/mpitest.cpp
+++ src/mpitest.cpp
@@ -15,23 +15,27 @@
 
 void master(int world_size, int argc, char** argv) {
     MPI_Status status;
+    char data[5];
     for (int i = 0; i < 7; ++i) {
         std::cout << "[master] Sleeping for 3 secs" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(3));
         std::cout << "[master] Waiting..." << std::endl;
-        MPI_Recv(nullptr, 0, MPI_BYTE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        MPI_Recv(data, 5, MPI_BYTE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
         std::cout << "[master] Received data from " << status.MPI_SOURCE << " with tag " << status.MPI_TAG << std::endl;
     }
 }
 
-bool slave(int world_rank) {
-    int sleep = world_rank * 3;
-    std::cout << "[slave] Sending some data to master with tag 1..." << std::endl;
-    MPI_Send(nullptr, 0, MPI_BYTE, 0, 1, MPI_COMM_WORLD);
+void slave(int world_rank) {
+    char data[5] = "test";
+    int sleep = 15 + world_rank * 3;
+    std::cout << "[slave] Sending 3x some data to master with tag 1..." << std::endl;
+    MPI_Send(data, 5, MPI_BYTE, 0, 100, MPI_COMM_WORLD);
+    MPI_Send(data, 5, MPI_BYTE, 0, 100, MPI_COMM_WORLD);
+    MPI_Send(data, 5, MPI_BYTE, 0, 100, MPI_COMM_WORLD);
     std::cout << "[slave] Sleeping for " << sleep << " secs..." << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(sleep));
     std::cout << "[slave] Sending some data to master with tag 3..." << std::endl;
-    MPI_Send(nullptr, 0, MPI_BYTE, 0, 3, MPI_COMM_WORLD);
+    MPI_Send(data, 5, MPI_BYTE, 0, 103, MPI_COMM_WORLD);
 }
 
 int main(int argc, char** argv) {
